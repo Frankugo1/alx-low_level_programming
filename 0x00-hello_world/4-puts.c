b493c1c0e4be89915bd1547ad41db087bf64d6c3
Frankugo1 <ugorji75@gmail.com>
2023-07-13 17:04:53 -0700

    It feels good,coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main A program that will print a line with function
+ * Return: 0 (readable)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
+
