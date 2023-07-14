8c8a5d2c313031abf96d4c6ff47e68e7d0219f9e
Frankugo1 <ugorji75@gmail.com>
2023-07-13 17:24:39 -0700

    It feels good,coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main A program that will print a line with function
+ * main - A program that will print a line with function
  * Return: 0 (readable)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
