147f30b884858dfff6804e24952b12aa9513e28e
Frankugo1 <ugorji75@gmail.com>
2023-07-16 15:27:11 -0700

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Point Entry
+ * Return: Always 0 Readable
+ */
+
+int main(void)
+{
+        char k;
+
+        k = 'z';
+        while
+                (k >= 'a') {
+                        putchar(k);
+                        k--;
+                }
+        putchar('\n');
+        return (0);
+}
