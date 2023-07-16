86ebaae32caf7c80762c1d9ee147fdffee091abd
Frankugo1 <ugorji75@gmail.com>
2023-07-16 15:54:08 -0700

    8-print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Point Entry
+ * Return: Always 0 Readable
+ */
+
+int main(void)
+{
+        char k;
+
+	int b;
+
+        k = 'a';
+	b = 0;
+        while
+                (b < 0) {
+                        putchar(b + '0');
+                        b++;
+                }
+	while
+		(k <= 'p') {
+			putchar(k);
+			k++;
+		}
+        putchar('\n');
+        return (0);
+}
